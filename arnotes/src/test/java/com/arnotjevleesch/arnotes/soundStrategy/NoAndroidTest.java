51e7086e459cea0ad9b0291ef1a9a5fd66913803
arno <arnaud.ruant@gmail.com>
2015-09-28 22:27:51 +0200

    non android test

---
+++ arnotes/src/test/java/com/arnotjevleesch/arnotes/soundStrategy/NoAndroidTest.java
@@ -0,0 +1,13 @@
+package com.arnotjevleesch.arnotes.soundStrategy;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class NoAndroidTest extends TestCase{
+
+    public void testToto() {
+        Assert.assertTrue(false);
+
+    }
+
+}
